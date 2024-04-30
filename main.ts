9639d23500c78bba64821323a9d123b63c1f4df3
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-04-30 13:54:58 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     getal += -1
-    basic.showNumber(getal)
 })
 input.onButtonPressed(Button.B, function () {
     getal += 1
-    basic.showNumber(getal)
 })
 let getal = 0
-getal = 0
 basic.showNumber(getal)
 basic.forever(function () {
-	
+    basic.showNumber(getal)
 })
