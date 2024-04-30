ec933d61d065f6162ad93ea2ace96cb40850fad4
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-04-30 13:42:11 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    getal += -1
+    basic.showNumber(getal)
+})
+input.onButtonPressed(Button.B, function () {
+    getal += 1
+    basic.showNumber(getal)
+})
+let getal = 0
+getal = 0
+basic.showNumber(getal)
+basic.forever(function () {
+    if (getal == 9) {
+        getal = 0
+    }
+})
